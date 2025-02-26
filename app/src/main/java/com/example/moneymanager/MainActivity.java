875d725e0c98b9f847ec08c81ff140fff0cf5c40
DESKTOP-3GT3IJ1\islab <t112598003@ntut.org.tw>
2025-02-26 12:37:39 +0800

    Adjust Navigation View and Font

--- app/src/main/java/com/example/moneymanager/MainActivity.java
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
